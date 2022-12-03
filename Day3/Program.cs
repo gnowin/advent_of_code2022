4aff6beef55a2098e79322b45386b96186b634e6
Albin Eddie <albineddie@gmail.com>
2022-12-03 13:37:50 +0100

    Day 3 Part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,36 @@
+﻿
+string inputPath = Environment.CurrentDirectory + "/input.txt";
+string[] rucksacks = File.ReadAllLines(inputPath);
+
+
+int sum = 0;
+foreach (var rucksack in rucksacks)
+{
+    int halfStringLength = rucksack.Length / 2;
+    string compartmentOne = rucksack.Substring(0, halfStringLength);
+    string compartmentTwo = rucksack.Substring(halfStringLength, halfStringLength);
+
+    foreach (var c in compartmentOne)
+    {
+        if (compartmentTwo.Contains(c))
+        {
+            sum += getPriority(c);
+            break;
+        }
+    }
+}
+
+Console.WriteLine(sum);
+
+
+static int getPriority(char letter)
+{
+    if (letter == char.ToLower(letter))
+    {
+        return (int)letter % (int)'a' + 1;
+    }
+    else
+    {
+        return (int)letter % (int)'A' + 27;
+    }
+}
