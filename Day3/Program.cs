40250a36bf502d56828bcd6515a542926e115c3c
Albin Eddie <albineddie@gmail.com>
2022-12-04 15:52:03 +0100

    Day3 Part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,9 +1,10 @@
 ﻿
+using Microsoft.VisualBasic.CompilerServices;
 string inputPath = Environment.CurrentDirectory + "/input.txt";
 string[] rucksacks = File.ReadAllLines(inputPath);
 
 
-int sum = 0;
+int sum = 0, sumPart2 = 0;
 foreach (var rucksack in rucksacks)
 {
     int halfStringLength = rucksack.Length / 2;
@@ -20,7 +21,20 @@ foreach (var rucksack in rucksacks)
     }
 }
 
+for (int i = 0; i < rucksacks.Count(); i += 3)
+{
+    HashSet<char> set1 = rucksacks[i].ToHashSet<char>();
+    HashSet<char> set2 = rucksacks[i + 1].ToHashSet<char>();
+    HashSet<char> set3 = rucksacks[i + 2].ToHashSet<char>();
+
+    set1.IntersectWith(set2);
+    set1.IntersectWith(set3);
+
+    sumPart2 += getPriority(set1.First());
+}
+
 Console.WriteLine(sum);
+Console.WriteLine(sumPart2);
 
 
 static int getPriority(char letter)
