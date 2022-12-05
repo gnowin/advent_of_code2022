d3bbfc396a869c1541bd8c1f2147772a6791a40f
gnowin <albineddie@gmail.com>
2022-12-05 18:48:28 +0100

    Day 5 part 1

--- C++/Day5/day5.cpp
+++ C++/Day5/day5.cpp
@@ -1,35 +1,88 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <list>
 
-int main()
+void moveFromTo(std::list<char> *from, std::list<char> *to, int amount)
 {
-    std::ifstream inputFile("input.txt");
-    std::string line;
-    std::vector<std::vector<int>> stacks;
-
-    for (size_t i = 0; i < 5; i++)
+    for (size_t i = 0; i < amount; i++)
     {
-        std::vector<int> stack;
-        stack.push_back(i);
-        stacks.push_back(stack);
-    }
-    for (size_t row = stacks.begin(); row != stacks.end(); row++)
-    {
-        std::vector<int> temp = stacks.at(row);
-        for (size_t col = temp.begin(); col != temp.end(); col++)
-        {
-            /* code */
-        }
-        }
-
-    while (getline(inputFile, line))
-    {
-        std::cout << line << std::endl;
+        to->push_back(from->back());
+        from->pop_back();
     }
 }
 
-void moveTopOfStack()
+int main()
 {
+    std::ifstream inputFile("input2.txt");
+    std::string line;
+    std::vector<std::list<char>> stacks;
+
+    int state = 0;
+    int columnSize = 4;
+    while (getline(inputFile, line))
+    {
+        int lineLength = line.length();
+        if (line.find('[') == std::string::npos)
+        {
+            state = 2;
+        }
+        if (state == 0)
+        {
+            for (size_t i = 0; i < (lineLength + 1) / columnSize; i++)
+            {
+                stacks.push_back(std::list<char>());
+            }
+            state++;
+        }
+        if (state <= 1)
+        {
+            for (size_t i = 0; i * columnSize < lineLength; i++)
+            {
+                char letter = line[1 + (i * columnSize)];
+                if (letter != ' ')
+                {
+                    stacks.at(i).push_front(letter);
+                }
+            }
+        }
+        else if (state > 1)
+        {
+            if (line.length() > 0 && line[0] != ' ')
+            {
+                std::stringstream text(line);
+                std::string segment;
+                std::vector<std::string> segments;
+
+                while (getline(text, segment, ' '))
+                {
+                    segments.push_back(segment);
+                }
+
+                int from = stoi(segments[3]);
+                int to = stoi(segments[5]);
+                int amount = stoi(segments[1]);
+
+                moveFromTo(&stacks[from - 1], &stacks[to - 1], amount);
+            }
+        }
+
+        // std::cout << line << std::endl;
+    }
+
+    std::string output;
+
+    for (size_t i = 0; i < stacks.size(); i++)
+    {
+        output += stacks[i].back();
+        for (std::list<char>::iterator it = stacks[i].begin(); it != stacks[i].end(); ++it)
+        {
+            // std::cout << *it;
+        }
+        // std::cout << std::endl;
+    }
+
+    std::cout << output << std::endl;
 }
